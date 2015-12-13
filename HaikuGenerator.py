1542f9bbf0df2962b182f42202aedb6bca056e25
Ellen O'Connor <eoconnor9484@gmail.com>
2015-12-13 08:23:29 -0800

    Added some code and changed some pseudocode

--- HaikuGenerator.py
+++ HaikuGenerator.py
@@ -1,47 +1,80 @@
-# Goal: Generate Haiku text using backtracking + Markov Chains
+# Goal: Generate Haiku text with Markov Chains
 
 # 1. Read in source text + make a dictionary of Markov chains
+def read_files(filenames):
+    """Given a list of files, make chains from them."""
 
-# 2. Get total syllable count for some list of words from CMUdict 
-#     e.g. ["I", "bought"] returns 2 
-#     In CMUdict, syllable stress is indicated with an integer on the vowel (e.g. AH1/EH2)
-#     so you can infer the number of syllables by looking at the number phones that
-#     end in an integer
+    body = ""
+
+    for filename in filenames:
+        text_file = open(filename)
+        body = body + text_file.read()
+        text_file.close()
+
+    return body
+
+def make_chains(text):
+    """Takes input text as string; returns dictionary of markov chains."""
+
+    chains = {}
+
+    words = corpus.split()
+
+    for i in range(len(words) - 2):
+        key = (words[i], words[i + 1])
+        value = words[i + 2]
+
+        if key not in chains:
+            chains[key] = []
+
+        chains[key].append(value)
+
+    return chains
+
+# Helper: get syll count 
+def count_syll(words):
+    """Get total syllable count for some list of words from CMUdict 
+    e.g. ["I", "bought"] returns 2 
+    In CMUdict, syllable stress is indicated with an integer on the vowel (e.g. AH1/EH2)
+    so you can infer the number of syllables by looking at the number phones that
+    end in an integer.
+
+    TODO"""
 
 # 3. Generate text: 
 
-# a. Select seed at random (constrained to those words appearing at the beginning
-#     of a sentence, i.e. starting with a capital letter. Example: ["I", "bought"])
-#     and constrained to word combinations with <= 5 syllables (for reasons related
-#     to the algorithm for get_lines)
+def get_seed(chains):
+    """Select seed at random (constrained to those words appearing at the beginning
+     of a sentence, i.e. starting with a capital letter. Example: ["I", "bought"])
+     and constrained to word combinations with <= 5 syllables ."""
 
-# b. get_lines
-#     (run 3 times- first line: 5 syllables, second line: 7 syllables,
-#     third line: 5 syllables. Each time the function gets called
-#     more words are added to the input.)
 
-#     Args:   list of words (default is random seed generated in 3a), 
-#             the number of syllables desired 
-#     Returns: a list of words (corresponding to the seed words, or else
-#             the haiku lines generated so far, if applicable)
+def get_lines(words, chains, target_syll):
+    """(run 3 times- first line: 5 syllables, second line: 7 syllables,
+    third line: 5 syllables. Each time the function gets called
+    more words are added to the input.)
 
-#     get syllable count of words
+    Args:   list of words (default is random seed generated in 3a), 
+            the number of syllables desired 
+    Returns: a list of words (corresponding to the seed words, or else
+            the haiku lines generated so far, if applicable)"""
 
-#     if syllable count is exactly 5
-#         return words                base case: gives us a full line
+    curr_syll = count_syll(words)
+    key = 
+    
+    while curr_syll < target_syll:
 
-#     w_candidates =  chains[w1, w2]
+        # So long as we haven't already popped off all of the candidate words (?) 
+        if len(chains[(words[-2], words[-1])]) > 0:     
+            # pop a random word from chains[(words[-2], words[-1])] & append it to words
+            curr_syll = count_syll(words)
+        else:
+            # remove the last word from words, chains
+            # update syllable count
 
-#     if syllable count < 5, add another word and recheck
+    if curr_syll == target_syll:  # base case: we have a complete line 
+        return words  
 
-#         pop a word from w_candidates and append it to words
-#         return get_lines(words)     check if syll count is 5 with new word
-
-#     else:
-#         return  
-
-#             (= if we have > 5 syllables, go back to the last string and reselect 
-#             a new word) 
-
-#             ??? How to make sure we don't keep selecting the same word over and
-#             over? (and if we exhaust all possible candidates we go up another node?)
+    elif curr_syll > target_syll:
+        # remove the last word from words
+        return get_lines(words, chains) 
